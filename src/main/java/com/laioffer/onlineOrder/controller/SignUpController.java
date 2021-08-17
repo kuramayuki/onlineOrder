eb0e8ee8ef5925d9311001631d8c2dfde4fe1e11
xuezhang <shirly_yukizhang@hotmail.com>
2021-08-16 20:58:28 -0700

    finish all function

--- src/main/java/com/laioffer/onlineOrder/controller/SignUpController.java
+++ src/main/java/com/laioffer/onlineOrder/controller/SignUpController.java
@@ -1,6 +1,8 @@
 package com.laioffer.onlineOrder.controller;
 
 import com.laioffer.onlineOrder.entity.Customer;
+import com.laioffer.onlineOrder.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class SignUpController {
+    @Autowired
+    private CustomerService customerService;
+
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public void signUp(@RequestBody Customer customer) {
-
+        customerService.signUp(customer);
     }
 }
